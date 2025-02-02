8438ccdd8ea6cd82db7b6ace5589658c10aa92f2
SaintEfim <saintefim2@gmai.com>
2025-02-02 14:48:16 +0300

    added create admin

---
+++ src/WC.Service.Authentication.AuthorizationAdmin/AuthorizationAdmin.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using WC.Service.Authentication.Domain.Models.AuthenticationLogin;
+using WC.Service.Authentication.Domain.Services;
+
+namespace WC.Service.Authentication.AuthorizationAdmin;
+
+public class AuthorizationAdmin
+{
+    private readonly IAuthenticationProvider _authenticationProvider;
+    private readonly ILogger<AuthorizationAdmin> _logger;
+
+    public AuthorizationAdmin(
+        ILogger<AuthorizationAdmin> logger,
+        IAuthenticationProvider authenticationProvider)
+    {
+        _authenticationProvider = authenticationProvider;
+        _logger = logger;
+    }
+
+    public async Task Create(
+        CancellationToken cancellationToken = default)
+    {
+        var emailLocalPart = Environment.GetEnvironmentVariable("ADMIN_EMAIL_LOCAL_PART") ?? "admin";
+        var emailDomain = Environment.GetEnvironmentVariable("ADMIN_EMAIL_DOMAIN") ?? "admin.com";
+
+        var authenticationLogin = new AuthenticationLoginRequestModel
+        {
+            Email = $"{emailLocalPart}@{emailDomain}",
+            Password = Environment.GetEnvironmentVariable("ADMIN_REGISTRATION_PASSWORD") ?? "Admin@12345678",
+        };
+
+        try
+        {
+            var response = await _authenticationProvider.Login(authenticationLogin, cancellationToken);
+
+            _logger.LogInformation("Registration successful");
+
+            _logger.LogInformation(
+                $"AccessToken: {response.AccessToken}\nRefreshToken: {response.RefreshToken}\nExpiresIn: {response.ExpiresIn}");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            throw;
+        }
+    }
+}
---
+++ src/WC.Service.Authentication.AuthorizationAdmin/Program.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WC.Service.Authentication.Domain;
+
+namespace WC.Service.Authentication.AuthorizationAdmin;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterInstance(LoggerFactory.Create(loggingBuilder => { loggingBuilder.AddConsole(); }))
+            .As<ILoggerFactory>()
+            .SingleInstance();
+
+        builder.RegisterGeneric(typeof(Logger<>))
+            .As(typeof(ILogger<>))
+            .SingleInstance();
+
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+        var basePath = AppDomain.CurrentDomain.BaseDirectory;
+        var projectPath = Path.Combine(basePath, "..", "..", "..");
+
+        var configuration = new ConfigurationBuilder().SetBasePath(projectPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        builder.RegisterInstance(configuration)
+            .As<IConfiguration>()
+            .SingleInstance();
+
+        builder.RegisterModule<AuthenticationDomainModule>();
+        builder.RegisterType<AuthorizationAdmin>();
+
+        var container = builder.Build();
+
+        await using var scope = container.BeginLifetimeScope();
+        var authorizationAdmin = scope.Resolve<AuthorizationAdmin>();
+        await authorizationAdmin.Create();
+    }
+}
